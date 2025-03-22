b800e33fe0f438b08800f0bf46ddfb94b9dbc646
Gavin Fox <gavingogaming@gmail.com>
2025-03-22 12:41:20 -0700

    Mobile nav

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { Button, IconButton } from "@mui/joy";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Navbar({activePage}: {activePage: string}) {
     const router = useRouter();
@@ -8,7 +10,11 @@ export default function Navbar({activePage}: {activePage: string}) {
             router.push(path);
         }
     }
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
+        <>
         <nav className='nav'>
             <div className="logo">
                 <img className="symbol" src='/Logo-Icon.png' alt='logo1' />
@@ -21,6 +27,23 @@ export default function Navbar({activePage}: {activePage: string}) {
                 <div onClick={go('/team')} className={`nav-item ${activePage=='team'?'active':''}`}>Team</div>
                 <div onClick={go('/contact')} className={`nav-item ${activePage=='contact'?'active':''}`}>Contact</div>
             </div>
+            <div className="mobile-flex">
+                <IconButton onClick={()=>{setMenuOpen(!menuOpen)}} color="neutral" sx={{color: menuOpen?'black':'white',background:menuOpen?'#7171ec':'#4d4da0',right:'15px',position:'absolute',top:'15px'}} className="menu-btn">
+                    <i className="fa-solid fa-bars"></i>
+                </IconButton>
+            </div>
         </nav>
+
+        {menuOpen && 
+            <div className="overlay-mobile-menu"></div>
+        }
+
+        {menuOpen && <div className="mobile-menu">
+            <div onClick={go('/')} className={`nav-item ${activePage=='home'?'active':''}`}> <i className="fa-solid fa-home"></i> Home</div>
+            <div onClick={go('/courses')} className={`nav-item ${activePage=='courses'?'active':''}`}><i className="fa-solid fa-scroll"></i> Courses</div>
+            <div onClick={go('/team')} className={`nav-item ${activePage=='team'?'active':''}`}><i className="fa-solid fa-people-group"></i> Team</div>
+            <div onClick={go('/contact')} className={`nav-item ${activePage=='contact'?'active':''}`}><i className="fa-solid fa-envelope"></i> Contact</div>
+        </div>}
+        </>
     );
 }
